f53a4b763e284edb5f9856b09f91152baca20fff
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-12-02 22:43:04 -0800

    fixed config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,17 +3,9 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/stylistic',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
+  parser: '@typescript-eslint/parser',  settings: { },
 }
